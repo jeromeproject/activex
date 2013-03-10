d4023e7b67ab3ed801a1a9a5c15bbfdbd016a086
verybada <verybada@verybada-VirtualBox.(none)>
2013-03-10 21:41:14 +0800

    Using walkaround to get file list

--- remote_player.js
+++ remote_player.js
@@ -1,10 +1,83 @@
-var player_index = 0;
+var PLAYER_INDEX = 0;
 var player;
+
+function pause()
+{
+	set_player_action("RPP_Pause_PI");
+}
+
+function play()
+{
+	set_player_action("RPP_Play_PI");
+}
+
+function playback()
+{
+	set_player_action("RPP_PlayBackward_PI");
+}
+
+function seekback()
+{
+	set_player_action("RPP_SeekBackward_PI");
+}
+
+function seekforward()
+{
+	set_player_action("RPP_SeekForward_PI");
+}
+
+function prevframe()
+{
+	set_player_action("RPP_PrevFrame_PI");
+}
+
+function nextframe()
+{
+	set_player_action("RPP_NextFrame_PI");
+}
+
+function seekbegin()
+{
+	set_player_action("RPP_SeekBegin_PI");
+}
+
+function seekend()
+{
+	set_player_action("RPP_SeekEnd_PI");
+}
+
+function set_player_action(action)
+{
+	// mapping to RPP_Command
+	var acts = new Object();
+	acts['RPP_Pause_PI'] = 12;
+	acts['RPP_PlayBackward_PI'] = 13;
+	acts['RPP_Play_PI'] = 14;
+	acts['RPP_SeekBackward_PI'] = 15;
+	acts['RPP_SeekForward_PI'] = 16;
+	acts['RPP_PrevFrame_PI'] = 17;
+	acts['RPP_NextFrame_PI'] = 18;
+	acts['RPP_SeekBegin_PI'] = 19;
+	acts['RPP_SeekEnd_PI'] = 20;
+	// end mapping
+	
+	player.ComPlayback(PLAYER_INDEX, acts[action], 0, 0);
+}
+
 function get_filelist(channel)
 {
-	var list = player.ComGetFileListVariant(player_index);
-	//alert(list);
-	return [ "fileA", "fileB", "fileC" ];
+	//alert("load list");
+	var list = player.ComGetFileListVariant(PLAYER_INDEX);
+	var filename_ary = new Array();
+	var s = new Array();
+	s = list.split("\n");
+	for(var i=0; i<s.length-1; i++)
+	{
+		var filename = player.ComGetFileName(PLAYER_INDEX, i, 0);
+		if(filename.length != 0)
+			filename_ary[i] = filename;
+	}
+	return filename_ary;
 }
 
 function get_channel()
@@ -17,25 +90,10 @@ function get_channel()
 	}
 }
 
-function pause()
-{
-	set_player_action(RPP_Pause_PI);
-}
-
-function play()
-{
-	set_player_action(RPP_Play_PI);
-}
-
-function set_player_action(action)
-{
-	player.ComPlayback(player_index, action, 0, 0);
-}
-
 function set_play_file(event, treeId, treeNode)
 {
-	var file_index = 0;
-	var ret = player.ComSelectFilevariant(player_index, file_index, 0);
+	var file_index = treeNode.tId.split("_")[1]-2;
+	var ret = player.ComSelectFilevariant(PLAYER_INDEX, file_index, 0);
 	if(ret.length == 0)
 	{
 		alert("Set file error.");
@@ -43,6 +101,19 @@ function set_play_file(event, treeId, treeNode)
 	else
 	{
 		play();
+	}
+}
+
+function load_filelist(channel)
+{
+	// load filelist
+	var filetree = get_tree("filetree");
+	var parent_node = filetree.getNodeByParam("name", "Filelist", null);
+	var list = get_filelist(channel);
+	var i;
+	for(i=0; i<list.length; i++)
+	{
+		tree_append_to_node(filetree, parent_node, list[i]);
 	}
 }
 
@@ -59,19 +130,11 @@ function init()
 		var port = parent.document.getElementById('port').innerHTML;
 		var user = parent.document.getElementById('user').innerHTML;
 		var passwd = parent.document.getElementById('passwd').innerHTML;
-		player.ComSetupConnect(player_index, address, port, user, passwd, undefined, undefined, undefined, undefined, channel);
+		player.ComSetupConnect(PLAYER_INDEX, address, port, user, passwd, undefined, undefined, undefined, undefined, channel);
 		//player.ComOpenPlayer();
-		player.ComEmbedPlayer(player_index);
-		
-		// load filelist
-		var filetree = get_tree("filetree");
-		var parent_node = filetree.getNodeByParam("name", "Filelist", null);
-		var list = get_filelist(channel);
-		var i;
-		for(i=0; i<list.length; i++)
-		{
-			tree_append_to_node(filetree, parent_node, list[i]);
-		}
+		player.ComEmbedPlayer(0);
+		//fixme: need sleep a while to get file list
+		window.setTimeout("load_filelist("+channel+");", 1000);
 	}
 	else
 	{
