6319d44eeead3c8a15cea5cca02ebaed893cdd1c
verybada <verybada@verybada-VirtualBox.(none)>
2013-03-27 00:39:19 +0800

    Update tree control functions.(add, delete, cancel, update)

--- js/index.js
+++ js/index.js
@@ -32,6 +32,56 @@ function get_server_info()
 function init()
 {
 	//window.setInterval("get_server_info()", 5000);
+	add_mode();
+}
+
+function reload_iframe()
+{
+	content.location.reload();
+}
+
+function set_button_hide(btn_id)
+{
+	document.getElementById(btn_id).style.display = 'none';
+}
+
+function set_button_show(btn_id)
+{
+	document.getElementById(btn_id).style.display = 'inline';
+}
+
+function add_mode()
+{
+	set_button_show('add');
+	set_button_hide('update');
+	set_button_hide('delete');
+	set_button_hide('cancel');
+}
+
+function update_mode()
+{
+	set_button_hide('add');
+	set_button_show('update');
+	set_button_show('delete');
+	set_button_show('cancel');
+}
+
+function clear_node_information()
+{
+	document.getElementById('new_address').value = "";
+	document.getElementById('new_port').value = "";
+	document.getElementById('new_user').value = "";
+	document.getElementById('new_passwd').value = "";
+}
+
+function show_node_information(event, treeid, node)
+{
+	var top_node = get_top_node(node);
+	document.getElementById('new_address').value = top_node.address;
+	document.getElementById('new_port').value = top_node.port;
+	document.getElementById('new_user').value = top_node.username;
+	document.getElementById('new_passwd').value = top_node.passwd;
+	update_mode();
 }
 
 function cancel()
@@ -40,9 +90,7 @@ function cancel()
 	document.getElementById('new_port').value = "";
 	document.getElementById('new_user').value = "";
 	document.getElementById('new_passwd').value = "";
-	document.getElementById('add').innerHTML = "Add";
-	document.getElementById('add').onClick = "add_server();";
-	document.getElementById('cancel').style.display = "none";
+	add_mode();
 }
 
 function add_server()
@@ -107,9 +155,33 @@ function add_server()
 		}
 	];
 	tree.addNodes(undefined, new_server, 0);
+	clear_node_information();
+	add_mode();
+}
 
-	document.getElementById('new_address').value = "";
-	document.getElementById('new_port').value = "";
-	document.getElementById('new_user').value = "";
-	document.getElementById('new_passwd').value = "";
+function update_server()
+{
+	var tree = get_tree('maintree');
+	var node = tree_get_current_node(tree);
+	node = get_top_node(node);
+	node.address = document.getElementById("new_address").value;
+	node.name = node.address;
+	node.port = document.getElementById("new_port").value;
+	node.username = document.getElementById("new_user").value;
+	node.passwd = document.getElementById("new_passwd").value;
+	tree.updateNode(node, true);
+	clear_node_information();
+	add_mode();
+	reload_iframe();
+}
+
+function delete_server()
+{	
+	var tree = get_tree('maintree');
+	var node = tree_get_current_node(tree);
+	node = get_top_node(node);
+	tree.removeChildNodes(node);
+	tree.removeNode(node);
+	clear_node_information();
+	add_mode();
 }
--- js/tree.js
+++ js/tree.js
@@ -19,30 +19,15 @@ function get_top_node(node)
 
 function get_server_info_with_select(tree)
 {
-	var node = tree.getSelectedNodes()[0];
+	var node = tree_get_current_node(tree);
 	node = get_top_node(node);
-	//while(typeof node.is_top == "undefined")
-	//{
-		//node = node.getParentNode();
-	//}
+
 	var info = new Array();
 	info[0] = node.address;
 	info[1] = node.port;
 	info[2] = node.username;
 	info[3] = node.passwd;
 	return info;
-}
-
-function show_node_information(event, treeid, node)
-{
-	var top_node = get_top_node(node);
-	document.getElementById('new_address').value = top_node.address;
-	document.getElementById('new_port').value = top_node.port;
-	document.getElementById('new_user').value = top_node.username;
-	document.getElementById('new_passwd').value = top_node.passwd;
-	document.getElementById('add').innerHTML = "Update";
-	document.getElementById('add').onClick = "alert('abc')";
-	document.getElementById('cancel').style.display = "block";
 }
 
 function tree_disable_collapse()
@@ -60,6 +45,7 @@ function tree_get_current_node(tree)
 	return tree.getSelectedNodes()[0];
 }
 
+// for file list
 var child_id = 0;
 function tree_append_to_node(tree, parent_node, child_name)
 {
